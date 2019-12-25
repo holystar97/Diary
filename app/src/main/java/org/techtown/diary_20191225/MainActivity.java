01fb7db71a995b73f5615684f893c3e832544227
holystar97 <mkrice500@gmail.com>
2019-12-25 22:56:45 +0900

    work

--- app/src/main/java/org/techtown/diary_20191225/MainActivity.java
+++ app/src/main/java/org/techtown/diary_20191225/MainActivity.java
@@ -1,5 +1,6 @@
 package org.techtown.diary_20191225;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -7,12 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView login_join;
     TextView login_find;
+    Button login_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        login_login=(Button) findViewById(R.id.login_login);
+        login_login.setOnClickListener(
+               new Button.OnClickListener(){
+                   @Override
+                   public void onClick(View view) {
+                       Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                       startActivity(intent);
+                   }
+               }
+        );
+
+
+
     }
 
     @Override
