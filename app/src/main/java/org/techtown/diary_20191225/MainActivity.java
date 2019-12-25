0cc51eb9c61437d658a8a99c4b3bdf3247b8ed37
holystar97 <mkrice500@gmail.com>
2019-12-25 21:03:46 +0900

    work

--- app/src/main/java/org/techtown/diary_20191225/MainActivity.java
+++ app/src/main/java/org/techtown/diary_20191225/MainActivity.java
@@ -1,13 +1,32 @@
 package org.techtown.diary_20191225;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView login_join;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       login_join=(TextView) findViewById(R.id.login_join);
+
+        login_join.setOnClickListener(
+                new TextView.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(getApplicationContext(),JoinActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+
+
+
     }
 }
