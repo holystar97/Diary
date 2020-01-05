b62e17f38089225e52534acb029ed58656f7f0b8
holystar97 <mkrice500@gmail.com>
2020-01-05 16:39:45 +0900

    db

--- app/src/main/java/org/techtown/diary_20191225/JoinActivity.java
+++ app/src/main/java/org/techtown/diary_20191225/JoinActivity.java
@@ -2,11 +2,14 @@ package org.techtown.diary_20191225;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Paint;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,10 +19,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class JoinActivity extends AppCompatActivity {
 
-    DBHelper dbHelper;
-    SQLiteDatabase database;
+    private static String IP_ADDRESS = "52.78.91.73/diary";
     Button join_join;
     Button checkBtn;
     private String tableName;
@@ -38,8 +47,6 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         user=new User();
-        dbHelper=new DBHelper(this);
-        db=dbHelper.getWritableDatabase();
 
         editText_id=findViewById(R.id.join_Id);
         editText_pw=findViewById(R.id.join_pw);
@@ -69,26 +76,7 @@ public class JoinActivity extends AppCompatActivity {
                 new Button.OnClickListener(){
                     @Override
                     public void onClick(View view) {
-                       if(checkId()) {
-                           AlertDialog.Builder builder=new AlertDialog.Builder(JoinActivity.this);
-                           builder.setMessage("사용 가능한 아이디 입니다.")
-                                   .setPositiveButton("확인",new DialogInterface.OnClickListener() {
-                                       @Override
-                                       public void onClick(DialogInterface dialogInterface, int i) {
-                                       }
-                                   }).show();
-                            isCheck=true;
-                       }
-                       else{
 
-                           AlertDialog.Builder builder=new AlertDialog.Builder(JoinActivity.this);
-                           builder.setMessage(alertNull)
-                                   .setPositiveButton("확인",new DialogInterface.OnClickListener() {
-                                       @Override
-                                       public void onClick(DialogInterface dialogInterface, int i) {
-                                       }
-                                   }).show();
-                       }
                     }
         }
         );
@@ -99,122 +87,96 @@ public class JoinActivity extends AppCompatActivity {
                 new Button.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (isCheck) {
-                            if (isNull()) {
-                                dbHelper.insertUser(user);
 
-                                AlertDialog.Builder builder=new AlertDialog.Builder(JoinActivity.this);
-                                builder.setMessage("가입이 완료되었습니다. 로그인 해주세요")
-                                        .setPositiveButton("확인",new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                                startActivity(intent);
-                                            }
-                                        }).show();
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
-                                builder.setMessage(alertNull)
-                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
+                        String id=editText_id.getText().toString();
+                        String password=editText_pw.getText().toString();
+                        String name=editText_name.getText().toString();
+                        String birth=editText_birth.getText().toString();
+                        String email=editText_email.getText().toString();
 
+                        InsertData tast= new InsertData();
+                        tast.execute("http://"+ IP_ADDRESS+ "/insert.php",id,password, name, birth,email);
 
-                                            }
-                                        }).show();
-                            }
-                        }else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
-                            builder.setMessage(alertNull)
-                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-
-
-                                        }
-                                    }).show();
-
-                        }
+                        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
                     }
+                });
+    }
+
+
+        class InsertData extends AsyncTask<String,Void, String>{
+
+            ProgressDialog progressDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog=ProgressDialog.show(JoinActivity.this,"기다려주세요",null,true,true);
+
+            }
+
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPreExecute();
+                progressDialog.dismiss();
+
+            }
+
+
+
+            @Override
+            protected String doInBackground(String... strings) {
+                String id=strings[1];
+                String password=strings[2];
+                String name=strings[3];
+                String birth=strings[4];
+                String email=strings[5];
+
+                String serverURL=strings[0];
+                String postParameters="id="+id+"&password="+password+"&name="+name+"&birth="+birth+"&email="+email;
+
+                try{
+                    URL uri=new URL(serverURL);
+                    HttpURLConnection httpURLConnection=(HttpURLConnection)uri.openConnection();
+                    httpURLConnection.setReadTimeout(5000);
+                    httpURLConnection.setConnectTimeout(5000);
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.connect();
+
+                    OutputStream outputStream=httpURLConnection.getOutputStream();
+                    outputStream.write(postParameters.getBytes("UTF-8"));
+                    outputStream.flush();
+                    outputStream.close();
+
+                    int responseStatusCode= httpURLConnection.getResponseCode();
+
+                    InputStream inputStream;
+                    if(responseStatusCode == HttpURLConnection.HTTP_OK){
+                        inputStream=httpURLConnection.getInputStream();
+                    }else{
+                        inputStream=httpURLConnection.getErrorStream();
+                    }
+
+                    InputStreamReader inputStreamReader=new InputStreamReader(inputStream,"UTF-8");
+                    BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+
+                    StringBuilder sb=new StringBuilder();
+                    String line=null;
+
+                    while((line = bufferedReader.readLine()) !=null){
+                        sb.append(this);
+                    }
+
+                    bufferedReader.close();
+                    return sb.toString();
+
+                }catch (Exception e) {
+                    return new String("Error: " + e.getMessage());
                 }
-        );
-
-    }
-
-    private void createDatabase() {
-        Toast.makeText(this, "호출됨", Toast.LENGTH_SHORT).show();
-
-        dbHelper = new DBHelper(this);
-        database = dbHelper.getWritableDatabase();
-
-
-        Toast.makeText(this, "생성함", Toast.LENGTH_SHORT).show();
-    }
-
-        private Boolean isNull(){
-        String pw=editText_pw.getText().toString().trim();
-        String name=editText_name.getText().toString().trim();
-        String birth=editText_birth.getText().toString().trim();
-        String email=editText_email.getText().toString().trim();
-
-        if(pw== null || pw.equals("")){
-            alertNull="비밀번호를 입력하세요";
-
-            return false;
-        }
-        else{
-            user.setPw(pw);
-        }
-        if(name== null || name.equals("")){
-            alertNull="이름을 입력하세요";
-
-            return false;
-        }
-        else{
-            user.setName(name);
-        }
-        if(birth== null || birth.equals("")){
-            alertNull="생년월일을 입력하세요";
-
-            return false;
-        }  else{
-            user.setBirth(birth);
-        }
-        if(email== null || email.equals("")){
-            alertNull="이메일을 입력하세요";
-
-            return false;
-        }  else{
-            user.setEmail(email);
-        }
-
-            return true;
-        }
-
-
-        private Boolean checkId(){
-            String id=editText_id.getText().toString().trim();
-            if(id== null ||id.equals("")){
-                alertNull="아이디를 입력하세요";
-                return false;
-            }
-            else{
-                user.setId(id);
             }
 
-            String sql="SELECT id FROM "+DBHelper.USER_TABLE_NAME;
-            Cursor cursor =db.rawQuery(sql,null);
-            if(cursor.moveToFirst()){
-                do{
-                    String userid=cursor.getString(0);
-                    if(userid.equals(id)){
-                        alertNull="중복되는 아이디입니다.";
-                        return false;
-                    }
-                }while(cursor.moveToNext());
-            }
-
-            return  true;
         }
 
 }
